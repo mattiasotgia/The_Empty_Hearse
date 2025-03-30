7ad1d67a5ea40b330f03567bd88e3927dcb9764e
Mattia Sotgia <msotgia@icarusgpvm03.fnal.gov>
2025-03-30 13:27:09 -0500

    misc

--- analysis/analysis_cosmic_id/cosmic_id.C
+++ analysis/analysis_cosmic_id/cosmic_id.C
@@ -13,8 +13,8 @@ void cosmic_id() {
 
     ana::SpectrumLoader loader10evt_noSAM_fullcheat("/exp/icarus/data/users/msotgia/thesis/stage1_runs_single/50evt_tests/test_4_jobsub/stage1_full_cheat_essential_50.flat.caf.root");
 
-    ana::SpectrumLoader loader10evt_noSAM_nocheat("/exp/icarus/data/users/msotgia/thesis/stage1_runs_single/50evt_tests/test_4_jobsub/stage1_no_cheat_essential_50.flat.caf.root");
-    
+    //    ana::SpectrumLoader loader10evt_noSAM_nocheat("/exp/icarus/data/users/msotgia/thesis/stage1_runs_single/50evt_tests/test_4_jobsub/stage1_no_cheat_essential_50.flat.caf.root");
+    ana::SpectrumLoader loader10evt_noSAM_nocheat("/pnfs/icarus/scratch/users/msotgia/bnb_nuonly_cheating/v09_89_01_01p03/production_for_analysis/stage1tocaf/cheated/reco_ana/out/fb/59/msotgia_v09_89_01_01p03_BNB_production_cheated_reco_ana_v09_89_01_01p03_stage1_cheated_18913322_0.flat.caf-745e4a70-db32-4719-8d1b-5ac4a394746f.root");
 
     ana::Spectrum dataNoCheated("", binsNorm, loader10evt_noSAM_nocheat, test, ana::kNoSpillCut);
     loader10evt_noSAM_nocheat.Go();
